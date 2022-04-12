e62beef0d75a7974109ede5e76b5d5fcba133434
Koffivi V. Woglo <59232299+VincentWoglo@users.noreply.github.com>
2022-04-11 20:02:27 -0600

    Add files via upload

--- Script/Animations.js
+++ Script/Animations.js
@@ -1,5 +1,11 @@
 Hero = document.getElementsByClassName("HeroText")
 NavigationContainer = document.getElementById("NavigationContainer")
+AboutMe = document.getElementById("AboutMe")
+PassionateText = document.getElementById("PassionateText")
+WorkExperienceSection = document.getElementById("WorkExperienceSection")
+ProjectSection = document.getElementById("ProjectSection")
+ContactSection = document.getElementById("ContactSection")
+
 gsap.from(Hero, {
     opacity: 0,
     duration: .9,
@@ -8,6 +14,13 @@ gsap.from(Hero, {
     delay: .050,
     ease: Power2.easeOut
 })
+gsap.from(PassionateText, {
+    opacity: 0,
+    duration: 1.3,
+    delay: .5,
+    y:-50,
+})
+
 gsap.from(NavigationContainer, {
     opacity: 0,
     duration: 1.3,
@@ -15,6 +28,38 @@ gsap.from(NavigationContainer, {
     y:0,
 })
 
-ScrollReveal().reveal('.AboutMe', { delay: 300 });
-ScrollReveal().reveal('.WorkExperienceSection', { delay: 300 });
-ScrollReveal().reveal('.ProjectSection', { delay: 300 });
+gsap.from(AboutMe, {
+    opacity: 0,
+    duration: .9,
+    y:-100,
+    stagger: 1,
+    delay: 3,
+    ease: Power2.easeOut
+})
+gsap.from(WorkExperienceSection, {
+    opacity: 0,
+    duration: .9,
+    y:-50,
+    stagger: 1,
+    delay: 3,
+    ease: Power2.easeOut
+})
+gsap.from(ProjectSection, {
+    opacity: 0,
+    duration: .9,
+    y:-100,
+    stagger: 1,
+    delay: 3,
+    ease: Power2.easeOut
+})
+gsap.from(ContactSection, {
+    opacity: 0,
+    duration: .9,
+    y:-100,
+    stagger: 1,
+    delay: 3,
+    ease: Power2.easeOut
+})
+ScrollReveal().reveal('.AboutMe', { delay: 500});
+ScrollReveal().reveal('.WorkExperienceSection', { delay: 500});
+ScrollReveal().reveal('.ProjectSection', { delay: 500});
--- ToDo.md
+++ ToDo.md
@@ -4,5 +4,3 @@
 - [] scroll to view project section
 - [] Responsive
 - [] Fix Smooth-Scroll JS scrolling bugs
-- [] Add resume download
-- [] Add contact

1c8c3fbbf1fc6f497d00cb811f0a82d35145465d
VincentWoglo <vincentwoglo@gmail.com>
2024-03-10 15:11:33 -0600

    YouTube hero edit

---
+++ assets/js/kw_js.js
@@ -0,0 +1,5 @@
+const youtube_btn = document.querySelector('.youtube-btn');
+
+youtube_btn.addEventListener('click', ()=>{
+    window.open('https://www.youtube.com/channel/UCAzFpJ-hFlig8K53tw00Bew', '_blank');
+})
